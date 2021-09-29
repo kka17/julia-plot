0a1f21b933cb099374625970bc61a58fa432e59f
kka17 <infokka17@gmail.com>
2021-09-29 18:00:19 +0300

    Growing circle

--- Animation.jl
+++ Animation.jl
@@ -1,6 +1,11 @@
-anim = @animate for i = 1:10
-    mat = rand(0:100, 32, 32)
-    heatmap(mat, clim=(0,255))
+a = 0
+b = 2π
+c = range(a, b, length = 100)
+
+
+anim = @animate for i = 1:100
+  plot(sin.(c)*(i+1), cos.(c)*(i+2))
 end
  
-gif(anim, "tutorial_heatmap_anim.gif", fps = 1)
+gif(anim, "tutorial_anim_fps30.gif", fps = 30)
+
