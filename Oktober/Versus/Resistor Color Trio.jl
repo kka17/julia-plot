17f228a55c8d2dd09009df699b0d22de9a7c6266
kka17 <infokka17@gmail.com>
2021-10-20 18:07:33 +0300

    RST

---
+++ Oktober/Versus/Resistor Color Trio.jl
@@ -0,0 +1,15 @@
+# Сопоставить входящее значение
+
+resistor's_colors = Dict(
+    "Black" => 0,
+    "Brown" => 1,
+    "Red" => 2,
+    "Orange" => 3,
+    "Yellow" => 4,
+    "Green" => 5,
+    "Blue" => 6,
+    "Violet" => 7,
+    "Grey" => 8,
+    "White" => 9
+)
+function 
